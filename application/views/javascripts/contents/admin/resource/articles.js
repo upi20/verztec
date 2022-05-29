a4ef795a5709c99bb6d012cf538fd95ea738b79c
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-05-30 02:58:08 +0700

    update articles

--- application/views/javascripts/contents/admin/resource/articles.js
+++ application/views/javascripts/contents/admin/resource/articles.js
@@ -1,9 +1,21 @@
 $(document).ready(() => {
     const table_html = $('#dt_basic');
     table_html.dataTable({
-        "responsive": false,
+        // "responsive": true,
         "lengthChange": true,
-        "autoWidth": false,
+        "autoWidth": true,
         "scrollX": true,
+        columnDefs: [{
+            targets: 5,
+            render: function (data, type, row) {
+                return `
+                <span class="d-inline-block" tabindex="0" data-toggle="tooltip" title="${data}">
+                ${data.substr(0, 50) + (data.length > 50 ? '...' : '')}
+                </span>
+                `;
+            }
+        }]
     });
+
+    $('.select2').select2();
 })
